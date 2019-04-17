43fc2b3458216105f9f784d846742deba8d9bc1a
Joey <jbahary2@illinois.edu>
2019-04-17 16:57:29 -0500

    Added cp3

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -4,10 +4,46 @@
 
 #include <mxnet/base.h>
 
+#define TILE_WIDTH 16
+
 namespace mxnet
 {
 namespace op
 {
+
+#if HELLO // macro that isnt defined
+    const int B = x.shape_[0]; // images in the batch
+    const int M = y.shape_[1]; // features to extract
+    const int C = x.shape_[1]; // channels for that one element (ex colors)
+    const int H = x.shape_[2]; // height of image
+    const int W = x.shape_[3]; // width of image
+    const int K = k.shape_[3]; // width of mask (mask is square)
+
+    for (int b = 0; b < B; ++b) {
+        
+        for (int m = 0; m < M; m++) {
+            // H - K + 1 is convolution size
+            for (int h = 0; h < H - K + 1; h++) {
+                // W - K + 1 is convolution size
+                for (int w = 0; w < W - K + 1; w++) {
+                    // index into image, then feature, and then y and x coords on the image
+                    y[b][m][h][w] = 0;
+                    // go over all the channels
+                    for (int c = 0; c < C; c++) {
+                        // finally compute this one mask. Jeeze
+                        for (int p = 0; p < K; p++) {
+                            for (int q = 0; q < K; q++) {
+                                // convolve with that mask
+                                y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+#endif
+
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
@@ -21,8 +57,6 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
@@ -31,7 +65,27 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    
+const int W_grid = ceil(W_out / (float)TILE_WIDTH);
+
+int b = blockIdx.x;
+int m = blockIdx.y;
+int h = blockIdx.z / W_grid + threadIdx.y;
+int w = blockIdx.z % W_grid + threadIdx.x;
+
+if (h >= H_out || w >= W_out) return;
+
+float accumulation = 0.0f;
+for (int c = 0; c < C; c++) {
+    for (int p = 0; p < K; p++) {
+        for(int q = 0; q < K; q++) {
+            accumulation += x4d(b, c, (h + p), (w + q)) * k4d(m, c, p, q); 
+        }
+    }
+}
+
+__syncthreads();
+
+y4d(b, m, h, w) = accumulation;
 
 #undef y4d
 #undef x4d
@@ -49,17 +103,30 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
-
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
+    const int B = x.shape_[0]; // images in the batch
+    const int M = y.shape_[1]; // features to extract
+    const int C = x.shape_[1]; // channels for that one element (ex colors)
+    const int H = x.shape_[2]; // height of image
+    const int W = x.shape_[3]; // width of image
+    const int K = w.shape_[3]; // width of mask (mask is square)
+
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+ 
+
+
+    const int W_grid = ceil(W_out / (float)TILE_WIDTH);
+    const int H_grid = ceil(H_out / (float)TILE_WIDTH);
+    const int Z = H_grid * W_grid; // number of tiles total
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    dim3 gridDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 blockDim(B, M, Z);
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
