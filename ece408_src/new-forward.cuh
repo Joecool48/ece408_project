96d5ee7ea0e3e72cccc86eba1485ad3400cea4d2
Nayman Leung <naymanl2@illinois.edu>
2019-04-18 00:50:29 -0500

    Added Nayman stuff

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -3,6 +3,8 @@
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 
 #include <mxnet/base.h>
+#define BLOCK_WIDTH 16
+
 
 namespace mxnet
 {
@@ -11,7 +13,7 @@ namespace op
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-
+    int n, m, c, h, w, p, q;
     /*
     Modify this function to implement the forward pass described in Chapter 16.
     We have added an additional dimension to the tensors to support an entire mini-batch
@@ -21,24 +23,52 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    //(void)H_out; // silence declared but never referenced warning. remove this line when you start working
+    //(void)W_out; // silence declared but never referenced warning. remove this line when you start working
+
+    //helps us index the pointers
+    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+    n = blockIdx.x; //idx of images
+    m = blockIdx.y; //idc of features
+    const int H_Grid = ceil(H_out/(float)BLOCK_WIDTH);
+    const int W_Grid = ceil(W_out/(float)BLOCK_WIDTH); //round up
+    h = blockIdx.z /(H_Grid)  * BLOCK_WIDTH + threadIdx.y; //idx of height of output tile
+    w = blockIdx.z % (W_Grid) * BLOCK_WIDTH + threadIdx.x; //idx of width of output tile
+
+    if(h >= H_out || w >= W_out)
+        return;
+
+    float accumulation = 0.0;
+
+    for (c = 0; c < C; c++){ //num of input feature maps
+        for (p = 0; p < K; p++){ //height idx of filter
+            for (q = 0; q < K; q++) { //width idx of filter
+                accumulation += x4d(n,c,h+p, w+q) * k4d(m,c,p,q);
+            }
+        }
+    }
+
+
+ __syncthreads();
+
+y4d(n,m,h,w) = accumulation;
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
 // y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    
+
+
 
 #undef y4d
 #undef x4d
 #undef k4d
 }
 
-/* 
+/*
    This function is called by new-inl.h
    Any code you write should be executed by this function.
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
@@ -49,24 +79,37 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+    //CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
+
+    const int B = x.shape_[0]; //number of output images
+    const int M = y.shape_[1]; //number of output feature maps
+    const int C = x.shape_[1]; //number of input feature maps
+    const int H = x.shape_[2]; //height of output elements
+    const int W = x.shape_[3]; //width of output element
+    const int K = w.shape_[3]; //dimension of the filters, width and height
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    const int H_out = H - K + 1; // the output after removing the edges
+    const int W_out = W - K + 1;
+
+    int W_grid =ceil(W_out/(float)BLOCK_WIDTH); // number of horizontal tiles per output map
+    int H_grid = ceil(H_out/(float)BLOCK_WIDTH); // number of vertical tiles per output map
+    int Z = H_grid * W_grid;
+
+    dim3 blockDim(BLOCK_WIDTH, BLOCK_WIDTH, 1);
+    dim3 gridDim(B, M, Z); //num of output images, number of output feature maps, total tiles
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
 }
 
-/* 
+/*
     This tells mxnet how to do an op when it's not a float.
     This is not used in the ECE408 project
 */
--- ece408_src/new-forward.h
+++ ece408_src/new-forward.h
@@ -1,4 +1,3 @@
-
 #ifndef MXNET_OPERATOR_NEW_FORWARD_H_
 #define MXNET_OPERATOR_NEW_FORWARD_H_
 
@@ -20,20 +19,35 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
     The goal here is to be correct, not fast (this is the CPU implementation.)
     */
 
-    const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = k.shape_[3];
+    const int B = x.shape_[0]; // images in the batch
+    const int M = y.shape_[1]; // features to extract
+    const int C = x.shape_[1]; // channels for that one element (ex colors)
+    const int H = x.shape_[2]; // height of image
+    const int W = x.shape_[3]; // width of image
+    const int K = k.shape_[3]; // width of mask (mask is square)
 
     for (int b = 0; b < B; ++b) {
 
-        CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
-
-        /* ... a bunch of nested loops later...
-            y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
-        */
+        for (int m = 0; m < M; m++) {
+            // H - K + 1 is convolution size
+            for (int h = 0; h < H - K + 1; h++) {
+                // W - K + 1 is convolution size
+                for (int w = 0; w < W - K + 1; w++) {
+                    // index into image, then feature, and then y and x coords on the image
+                    y[b][m][h][w] = 0;
+                    // go over all the channels
+                    for (int c = 0; c < C; c++) {
+                        // finally compute this one mask. Jeeze
+                        for (int p = 0; p < K; p++) {
+                            for (int q = 0; q < K; q++) {
+                                // convolve with that mask
+                                y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
